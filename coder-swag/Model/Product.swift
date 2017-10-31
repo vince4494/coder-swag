9dff8a91562728eeef7b762fcc5a9af2508bb5ae
vince zipparro <vincezipparro@vinces-MacBook-Pro.local>
2017-10-31 13:26:13 -0500

    Created Product struct in Model folder and set up DataService file to handle product data.

---
+++ coder-swag/Model/Product.swift
@@ -0,0 +1,34 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by vince zipparro on 10/31/17.
+//  Copyright © 2017 vince zipparro. All rights reserved.
+//
+
+import Foundation
+
+struct Product
+{
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName:String)
+    {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
