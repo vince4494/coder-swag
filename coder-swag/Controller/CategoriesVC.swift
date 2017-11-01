af5606fdbe99fa9eb1e9484ccce5e9e516635319
vince zipparro <vincezipparro@vinces-mbp.lan>
2017-10-31 20:48:10 -0500

    Second view controller completed. First view controller is now showing images but padding between cells is not there. Collection views displaying data.

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -45,6 +45,29 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         }
     }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let productsVC = segue.destination as? ProductsVC
+        {
+            //setting back button text to be an empty string
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            //replace whats there with something new
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+            
+        }
+        
+    }
 
 }
 
